b0374a3bde1ddcaea55e5929d2d1314245bcdd1d
moses okonkwo <okonkwomoses158@gmail.com>
2025-12-22 06:56:55 +0100

    feat: add Footer component

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,7 @@
-export default function Footer(){
-    return(
+export default function Footer() {
+    return (
         <footer className="  p-4 text-center flex justify-center items-center border-t-2">
-            <p className=" font-fira ">&copy; 2025 MOZ. All rights reserved.</p>
+            <p className=" font-fira ">&copy; {new Date().getFullYear()} MOZ. All rights reserved.</p>
         </footer>
     )
 }
